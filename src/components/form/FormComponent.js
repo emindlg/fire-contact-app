52d86d5f525c5288f5d75395eb5b24921777fdde
emindlg <94062446+emindlg@users.noreply.github.com>
2022-06-01 15:12:29 +0300

    final commit

--- src/components/form/FormComponent.js
+++ src/components/form/FormComponent.js
@@ -35,12 +35,12 @@ const FormComponent = ({info,setInfo, handleFormSubmit}) => {
       <p className="contact-header">
         <div>
           <a
-            href="https://github.com/clarusway"
+            href="https://github.com/emindlg"
             className="design"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <code>{"<Clarusway/> "}</code>
+            <code>{"My Github"}</code>
           </a>
         </div>
         <span className="design header">design</span>
